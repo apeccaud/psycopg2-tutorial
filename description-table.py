999e72f36ab93942ea5fc68934cd9697083ac23e
Alexandre PECCAUD <alexandre.peccaud@gmail.com>
2018-03-31 22:37:48 +0200

    Describe all tables in database

--- description-table.py
+++ description-table.py
@@ -1,7 +1,12 @@
 import psycopg2
+from psycopg2.extensions import AsIs
 
-conn = psycopg2.connect("dbname=dq user=dq")
+conn = psycopg2.connect(dbname="dq", user="hud_admin", password="eRqg123EEkl")
 cur = conn.cursor()
 
-cur.execute("SELECT * FROM ign_reviews LIMIT 0")
-print(cur.description)
+cur.execute("SELECT table_name FROM information_schema.tables WHERE table_schema='public'")
+for table in cur.fetchall():
+    statement = cur.mogrify("SELECT * FROM %s LIMIT 0", [AsIs(table[0])])
+    cur.execute(statement)
+    print(cur.description)
+    print(" ")
