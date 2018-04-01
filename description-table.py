4fab53a17700084ebd9fa816d4f61cc6940534c9
Alexandre PECCAUD <alexandre.peccaud@gmail.com>
2018-04-01 18:02:33 +0200

    description-table : Change list by tuple

--- description-table.py
+++ description-table.py
@@ -6,7 +6,7 @@ cur = conn.cursor()
 
 cur.execute("SELECT table_name FROM information_schema.tables WHERE table_schema='public'")
 for table in cur.fetchall():
-    statement = cur.mogrify("SELECT * FROM %s LIMIT 0", [AsIs(table[0])])
+    statement = cur.mogrify("SELECT * FROM %s LIMIT 0", (AsIs(table[0]), ))
     cur.execute(statement)
     print(cur.description)
     print(" ")
