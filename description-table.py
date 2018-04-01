60f652f532ab679d7d7bcce50e71fb58deffa36a
Alexandre PECCAUD <alexandre.peccaud@gmail.com>
2018-04-01 18:33:12 +0200

    Improve description-table.py

--- description-table.py
+++ description-table.py
@@ -1,12 +1,34 @@
 import psycopg2
 from psycopg2.extensions import AsIs
 
-conn = psycopg2.connect(dbname="dq", user="hud_admin", password="eRqg123EEkl")
+conn = psycopg2.connect(dbname="", user="hud_admin", password="eRqg123EEkl")
 cur = conn.cursor()
 
+# Get table mapping type_code and type_name
+cur.execute("SELECT oid, typname FROM pg_catalog.pg_type")
+type_mappings = {
+    int(oid): typename
+    for oid, typename in cur.fetchall()
+}
+
+# Get table names
 cur.execute("SELECT table_name FROM information_schema.tables WHERE table_schema='public'")
-for table in cur.fetchall():
-    statement = cur.mogrify("SELECT * FROM %s LIMIT 0", (AsIs(table[0]), ))
+table_names = cur.fetchall()
+
+# Get readable description of the tables
+readable_description = {}
+for table in table_names:
+    statement = cur.mogrify("SELECT * FROM %s LIMIT 0", (AsIs(table), ))
     cur.execute(statement)
-    print(cur.description)
-    print(" ")
+    readable_description[table] = {
+        "columns": [
+            {
+                "name": col.name,
+                "type": type_mappings[col.type_code],
+                "internal_size": col.internal_size
+            }
+            for col in cur.description
+        ]
+    }
+
+print(readable_description)
